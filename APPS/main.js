ac3418a699ba0b7d392d6954c680530b7ac72ac7
Moses Onyango <mosesonyango724@gmail.com>
2024-08-03 20:03:00 +0300

    first commit

---
+++ APPS/main.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var navLinks = document.getElementById('nav-link');
+    navLinks.classList.toggle('show');
+}
+
+function hideMenu() {
+    var navLinks = document.getElementById('nav-link');
+    navLinks.classList.remove('show');
+}
